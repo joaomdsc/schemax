02407f761bfe9e855948849cd7ae085567e0273f
João Moreira de Sá Coutinho <joao.moreiradsc@gmail.com>
2021-11-27 19:04:34 +0100

    First verison of gen_pyclasses.py

--- pyclass/pyclass_xsd.py
+++ pyclass/pyclass_xsd.py
@@ -10,7 +10,6 @@ and we want to json-serialize them, so we want to implement dictify().
 import sys
 from keyword import iskeyword
 import builtins
-from operator import attrgetter
 
 #-------------------------------------------------------------------------------
 
@@ -41,33 +40,6 @@ class PyClassXsd(PyClass):
         self.dictify = PyDictifyFunc(self.__class__, self.args, \
                                      parent=self.parent, level=1)
 
-    def param_names(self):
-        """Names of self's own parameters to init(), required and optional"""
-        return {
-            'required': [p.name for p in self.args if not p.optional],
-            'optional': [p.name for p in self.args if p.optional],
-        }
-
-    def all_param_names():
-        """Parameters to init() from self and all of its ancestors"""
-        # Get the list of classes that contribute parameters to init, i.e. self
-        # plus all its ancestors, if any.
-
-        # FIXME my params + parent.all_param_names(). We never need ancestry,
-        # just recall the function recursively.
-        klasses = [self, self.parent] + self.parent.ancestry() \
-            if self.parent is not None else [self]
-
-        # I've lost the distinction between self and ancestors
-        req = []
-        opt = []
-        for c in klasses:
-            p = c.param_names()
-            req.extend(p['required'])
-            opt.extend(p['optional'])
-
-        return req, opt
-
     #---------------------------------------------------------------------------
 
     def __str__(self):
@@ -79,16 +51,18 @@ class PyClassXsd(PyClass):
 
     #---------------------------------------------------------------------------
 
+    # Here we define gen_dictify as a method of the PyClass subclass, instead
+    # of defining
     def gen_dictify(self):
         """Return the source code for this class's dictify() function.
         """
         s = f"""
-    def dictify(self):
-        {quot3}Return a json-encodable object representing a {self.name}.{quot3}
-        obj = {{}}
-        for k, v in self.__dict__.items():
-            if not (v is None or v == [] or v == {{}}):
-                obj[k] = v
+def dictify(self):
+    {quot3}Return a json-encodable object representing a {self.name}.{quot3}
+    obj = {{}}
+    for k, v in self.__dict__.items():
+        if not (v is None or v == [] or v == {{}}):
+            obj[k] = v
 """
         # If some of the arguments are python objects rather than simple types,
         # arrays, dictionaries, or combinations thereof, then they must also be
@@ -122,50 +96,6 @@ class PyClassXsd(PyClass):
     def __str__(self):
         return json.dumps(self.dictify(), indent=4)
 """
-        return s
-
-#-------------------------------------------------------------------------------
-
-class PyInitFunc(PyFunction):
-    def __init__(self, klass, args, parent=None, level=None):
-        super_args = [PyArg('self', klass, optional=False)] + args
-        super().__init__('__init__', super_args, level=level)
-        self.parent = parent
-
-    def __str__(self):
-        # Generate the function signature 
-        s = self.gen_signature()
-
-        # If it's a derived class, call superclass's __init__
-        if self.parent is not None:
-            s += f'{ind*(self.level+1)}super().__init__(self'
-            # Here we need to include only the ancestor's parameters, required
-            # first, followed by optional. Initially on a single line.
-            req, opt = self.parent.all_param_names():
-            for p in req:
-                s += f', {p}'
-            for p in opt:
-                s += f', {p}={p}'
-            s += '\n'
-
-        # Mutables: when generating from XML Schema, list is the only possible
-        # mutable, in fact mutable == card_many == list.
-
-        # Set member values
-        for a in self.args[1:]:
-            # Here we need to include only self's parameters 
-            if a.type_ in [dict, list]:
-                # But we need to distinguish mutable or not
-                s += '\n'
-                s += f'{ind*2}self.{a.name} = {"[]" if a.type_ == list \
-                    else "{}"}\n'
-                s += f'{ind*2}if {a.name} is not None:\n'
-                s += f'{ind*3}self.{a.name} = {a.name}\n'
-            else:
-                s += f'{ind*2}self.{a.name} = {a.name}\n'
-        if len(self.args) == 0:
-            s += f'{ind*2}pass\n'
-
         return s
 
 #-------------------------------------------------------------------------------
