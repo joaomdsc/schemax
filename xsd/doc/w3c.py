48457c6bdb7e3bd49e438cce96cbbdbd8048e517
João Moreira de Sá Coutinho <joao.moreiradsc@gmail.com>
2021-11-28 15:48:00 +0100

    Get headings and some refs in w3c.py

--- xsd/doc/w3c.py
+++ xsd/doc/w3c.py
@@ -1,5 +1,7 @@
 # w3c.py - convert W3C recommendations in HTML format to a Word file
 
+import os
+import re
 import sys
 import lxml.etree as et
 from lxml import objectify
@@ -19,39 +21,122 @@ red = RGBColor(0xff, 0x0, 0x0)
 success = RGBColor(0xde, 0xf1, 0xde)  # (222, 241, 222) dec
 warning = RGBColor(0xfc, 0xef, 0xdc)  # (252, 239, 220) dec
 
+#-------------------------------------------------------------------------------
+
+def lcoalesce(s):
+    """Coalesce multiple whitespace characters into a single space char."""
+    # return re.sub(r'^\s+', ' ', s)
+    return re.sub(r'\s+', ' ', s)
+
+def rcoalesce(s):
+    # return re.sub(r'\s+$', ' ', s)
+    return re.sub(r'\s+', ' ', s)
+
 # -----------------------------------------------------------------------------
 
-def handle_para(nd, doc):
-    s = ''
+def get_i_text(nd, p, bold=None, italic=None, strike=None):
+    # Text before any eventual sub-element. See get_h2 for additional info.
+    # This is an element inside a <strong>, <em>, or <del>
+    if nd.text:
+        s = nd.text
+        if len(s) > 0:
+            if s[0] not in ' \t\r\n':
+                # We're assuming this is not at the beginning of a paragraph,
+                # there's some other text *before*, and we want to separate
+                # ourselves from it with at least one space.
+                s = ' ' + s
+            else:
+                s = lcoalesce(s)
+        if len(nd) == 0:
+            s = s.rstrip()
+        if len(s) > 0:
+            add_text_run(p, s, bold=bold, italic=italic, strike=strike)
 
-    if nd.text is not None:
-        s += nd.text.lstrip()
+    # Process any eventual sub-elements.
     for k in nd:
-        if k.text is not None:
-            s += k.text.lstrip()
-        if k.tail is not None:
-            s += k.tail.rstrip()
+        s = k.text.strip() if k.text else ''
+        if k.tag == 'i' and len(s) > 0:
+            add_text_run(p, s, bold=bold, italic=italic, strike=strike)
+        if k.tail:
+            # Note k.tail only gets stripped on the right side, not the
+            # left. On the left side we lcoalesce, assuming there was something
+            # else before us.
+            s = k.tail.rstrip()
+            s = lcoalesce(s)
+            if len(s) > 0:
+                add_text_run(p, s, bold=bold, italic=italic, strike=strike)
 
-    # Output a text paragraph to the document
+#-------------------------------------------------------------------------------
+
+def handle_para(nd, doc, refs):
+    # One paragraph for everyting
     p = new_paragraph(doc)
-    add_text_run(p, s)
+
+    # Text before any eventual sub-element. Left-strip always, because HTML
+    # authors usually add whitespace for legibility. Right-strip (for the same
+    # reason) but only if there are no sub-elements, otherwise we might
+    # concatenate two words together.
+    if nd.text is not None:
+        s = nd.text.lstrip()
+        if len(nd) == 0:
+            s = rcoalesce(s)
+        else:
+            # There are sub-elements, don't just strip, coalesce whitespace
+            s = rcoalesce(s)
+        if len(s) > 0:
+            add_text_run(p, s)
+            
+    # Process any eventual sub-elements.
+    for k in nd:
+        s = k.text.strip() if k.text else ''
+        if k.tag in ['specref']:
+            ref = k.attrib['ref']
+            add_text_run(p, refs[ref])
+        elif k.tag == 'emph':
+            get_i_text(k, p, italic=True)
+        if k.tail:
+            # Note k.tail only gets stripped on the right side, not the
+            # left. On the left side we lcoalesce, assuming there was something
+            # else before us.
+            s = k.tail.rstrip()
+            s = lcoalesce(s)
+            if len(s) > 0:
+                add_text_run(p, s)
 
 # -----------------------------------------------------------------------------
 
-def handle_div(nd, doc):
+def handle_div(nd, doc, refs):
     level = int(nd.tag[3])  # 1, 2, 3 or 4
     id_ = nd.attrib['id'] if 'id' in nd.attrib else None
 
-    print(f'<div{level} id="{id_}">')
+    # print(f'<div{level} id="{id_}">')
     for k in nd:
         if k.tag == 'head':
             title = k.text.strip() if k.text is not None else '<empty'
-            print(f'  title="{title}"')
+            # print(f'  title="{title}"')
             add_heading(doc, level, title)
         elif k.tag == 'p':
-            handle_para(k, doc)
+            handle_para(k, doc, refs)
         elif k.tag.startswith('div'):
-            handle_div(k, doc)
+            handle_div(k, doc, refs)
+
+# -----------------------------------------------------------------------------
+
+def get_specrefs(nd, obj):
+    id_ = None
+    for k in nd:
+        if k.tag == 'head':
+            continue
+        elif k.tag.startswith('div') or k.tag in ['schemaComp', 'constraintnote']:
+            id_ = k.attrib['id'] if 'id' in k.attrib else None
+            x = k.find('.//head')
+            title = x.text.strip() if x.text is not None else '<empty>'
+            obj[id_] = title
+            obj = get_specrefs(k, obj)
+        else:
+            obj = get_specrefs(k, obj)
+
+    return obj
 
 # -----------------------------------------------------------------------------
 
@@ -59,17 +144,21 @@ def to_docx(filepath):
     # Parse XML file, ignoring comments
     p = et.XMLParser(remove_comments=True)
     root = objectify.parse(filepath, parser=p).getroot()
-    nd = root.find('.//body')
-    print(nd.tag)
+    print(root.tag)
+
+    specrefs = get_specrefs(root, {})
+    for k, v in specrefs.items():
+        print(f'{k}: {v}')
 
     # Write out the contents as Word
     doc = Document('empty.docx')
 
     # Get the document contents
+    nd = root.find('.//body')
     for k in nd:
         if not k.tag.startswith('div') or k.tag[3] not in ['1', '2', '3', '4']:
             continue
-        handle_div(k, doc)
+        handle_div(k, doc, specrefs)
 
     # Set normal margins
     s = doc.sections[0]
@@ -79,7 +168,8 @@ def to_docx(filepath):
     s.bottom_margin = Inches(0.59)
           
     # Output a .docx file in the current directory
-    outpath = 'xsd1_structures.docx'
+    base, _ = os.path.splitext(filepath)
+    outpath = f'{base}.docx'
     doc.save(outpath)
     
 # -----------------------------------------------------------------------------
